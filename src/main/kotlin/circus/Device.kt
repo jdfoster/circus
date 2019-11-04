20e6125028e1ce70c488c9b471d214484ce4767c
Joshua Foster <joshua.david.foster@gmail.com>
2019-11-04 18:25:10 +0000

    feat: add temperature record protocol

--- src/main/kotlin/circus/Device.kt
+++ src/main/kotlin/circus/Device.kt
@@ -8,12 +8,13 @@ import akka.actor.typed.javadsl.ActorContext
 import akka.actor.typed.javadsl.Behaviors
 import akka.actor.typed.javadsl.Receive
 
-
 class Device(private val context: ActorContext<DeviceMessage>, groupId: String, deviceId: String): AbstractBehavior<Device.Factory.DeviceMessage>() {
     companion object Factory {
         interface DeviceMessage {}
         data class ReadTemperature(val requestId: Long, val replyTo: ActorRef<RespondTemperature>): DeviceMessage
         data class RespondTemperature(val requestId: Long, val value: Double?)
+        data class RecordTemperature(val requestId: Long, val value: Double, val replyTo: ActorRef<TemperatureRecorded>): DeviceMessage
+        data class TemperatureRecorded(val requestId: Long)
 
         fun createBehaviour(groupId: String, deviceId: String): Behavior<DeviceMessage> {
             return Behaviors.setup{ Device(it, groupId, deviceId) }
@@ -29,12 +30,20 @@ class Device(private val context: ActorContext<DeviceMessage>, groupId: String, 
     override fun createReceive(): Receive<DeviceMessage> {
         return newReceiveBuilder()
                 .onMessage(ReadTemperature::class.java) { readTemperature(it) }
+                .onMessage(RecordTemperature::class.java) { recordTemperature(it) }
                 .onSignal(PostStop::class.java) { postStop() }
                 .build()
     }
 
     private fun readTemperature(req: ReadTemperature): Behavior<DeviceMessage> {
         req.replyTo.tell(RespondTemperature(req.requestId, lastTemperatureReading))
+        return this
+    }
+
+    private fun recordTemperature(req: RecordTemperature): Behavior<DeviceMessage> {
+        context.log.info("Recorded temperature reading {} with {}", req.value, req.requestId)
+        lastTemperatureReading = req.value
+        req.replyTo.tell(TemperatureRecorded(req.requestId))
         return this
     }
 
--- src/test/kotlin/circus/DeviceTest.kt
+++ src/test/kotlin/circus/DeviceTest.kt
@@ -3,6 +3,7 @@ package circus
 import akka.actor.testkit.typed.javadsl.BehaviorTestKit
 import akka.actor.testkit.typed.javadsl.TestInbox
 import io.kotlintest.extensions.TestListener
+import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
 
 class DeviceTest: StringSpec() {
@@ -14,6 +15,27 @@ class DeviceTest: StringSpec() {
             val inbox = TestInbox.create<Device.Factory.RespondTemperature>()
             test.run(Device.Factory.ReadTemperature(42L, inbox.ref))
             inbox.expectMessage(Device.Factory.RespondTemperature(42L, null))
+        }
+
+        "reply with latest temperature reading" {
+            val recordProbe = ActorListener.actorTestKit.createTestProbe<Device.Factory.TemperatureRecorded>()
+            val readProbe = ActorListener.actorTestKit.createTestProbe<Device.Factory.RespondTemperature>()
+            val deviceActor = ActorListener.actorTestKit.spawn(Device.Factory.createBehaviour("group", "device"))
+            deviceActor.tell(Device.Factory.RecordTemperature(1L, 24.0, recordProbe.ref))
+            recordProbe.expectMessage(Device.Factory.TemperatureRecorded(1L))
+
+            deviceActor.tell(Device.Factory.ReadTemperature(2L, readProbe.ref))
+            val responseOne = readProbe.receiveMessage()
+            responseOne.requestId.shouldBe(2L)
+            responseOne.value.shouldBe(24.0)
+
+            deviceActor.tell(Device.Factory.RecordTemperature(3L, 55.0, recordProbe.ref))
+            recordProbe.expectMessage(Device.Factory.TemperatureRecorded(3L))
+
+            deviceActor.tell(Device.Factory.ReadTemperature(4L, readProbe.ref))
+            val responseTwo = readProbe.receiveMessage()
+            responseTwo.requestId.shouldBe(4L)
+            responseTwo.value.shouldBe(55.0)
         }
     }
 }
